05c472272813bf1da2786a26c7f2e5fc956bc6af
ccherrerab <ccherrera913@ucatolica.edu.co>
2025-09-16 09:50:14 -0500

    Second COMMIT, CSS y HTML.blade.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('login');
+})->name('login');
+
+Route::get('/signIn', function () {
+    return view('signIn');
+})->name('signIn');
