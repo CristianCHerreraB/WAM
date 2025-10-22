99544e9df98939c358e18a21c2dc576af2999972
ccherrerab <ccherrera913@ucatolica.edu.co>
2025-10-22 16:48:18 -0500

    nuevo2

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,8 @@ Route::get('/', function () {
 
 
 
-/*Route::get('/signIn', function () {
-    return view('signIn');
-})->name('signIn');*/
-
 // Página principal
 Route::get('/dashboard', [PageController::class, 'dashboard'])->name('dashboard');
-// Rutas para las diferentes páginas
-//Route::get('/dashboard', [PageController::class, 'dashboard'])->name('dashboard');
 Route::get('/descubre', [PageController::class, 'descubre'])->name('descubre');
 Route::get('/biblioteca', [PageController::class, 'biblioteca'])->name('biblioteca');
 Route::get('/informes', [PageController::class, 'informes'])->name('informes');
@@ -34,12 +28,6 @@ Route::get('/password', [PageController::class, 'password'])->name('password');
 Route::get('/otras-apps', [PageController::class, 'otrasApps'])->name('otras-apps');
 Route::get('/ayuda', [PageController::class, 'ayuda'])->name('ayuda'); 
 
-// Página principal
-//Route::get('/dashboard', [PageController::class, 'dashboard'])->name('dashboard');
-/*Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-*/
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
