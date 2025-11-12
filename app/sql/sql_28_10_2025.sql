a9c45316cc40e3668a6b4c156e7f536668639f60
ccherrerab <ccherrera913@ucatolica.edu.co>
2025-11-12 15:03:54 -0500

    act-login

--- app/sql/sql_28_10_2025.sql
+++ app/sql/sql_28_10_2025.sql
@@ -1,3 +1,13 @@
 /*Se agrega el campo id_ejecucion a la tabla cal_juez*/
 ALTER TABLE cal_juez
 ADD COLUMN id_ejecucion INT;
+
+/*login*/
+ALTER TABLE [dbo].[usuarios]
+ADD 
+    [user_facebook] VARCHAR(255) NOT NULL,
+    [user_instagram] VARCHAR(255) NOT NULL,
+    [user_x] VARCHAR(255) NOT NULL,
+    [telefono] VARCHAR(20) NOT NULL,
+    [genero] VARCHAR(10) NOT NULL,
+    [edad] INT NOT NULL;
