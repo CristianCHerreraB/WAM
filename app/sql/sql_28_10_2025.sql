6b97f151c43cab5a2e147d7f12b1583a62d5130a
ccherrerab <ccherrera913@ucatolica.edu.co>
2025-11-12 18:35:46 -0500

    12-11/2025

--- app/sql/sql_28_10_2025.sql
+++ app/sql/sql_28_10_2025.sql
@@ -3,12 +3,49 @@ ALTER TABLE cal_juez
 ADD COLUMN id_ejecucion INT;
 
 /*login*/
+-- 1️⃣ Agregamos las columnas permitiendo NULL 
 ALTER TABLE [dbo].[usuarios]
-ADD 
-    [user_facebook] VARCHAR(255) NOT NULL,
-    [user_instagram] VARCHAR(255) NOT NULL,
-    [user_x] VARCHAR(255) NOT NULL,
-    [telefono] VARCHAR(20) NOT NULL,
-    [genero] VARCHAR(10) NOT NULL,
-    [edad] INT NOT NULL;
+ADD
+    [user_facebook] VARCHAR(255) NULL,
+    [user_instagram] VARCHAR(255) NULL,
+    [user_x] VARCHAR(255) NULL,
+    [telefono] VARCHAR(20) NULL,
+    [genero] VARCHAR(10) NULL,
+    [edad] INT NULL;
+
+-- 2️⃣ Actualizamos los registros existentes con valores por defecto
+UPDATE [dbo].[usuarios]
+SET
+    [user_facebook] = 'no aplica',
+    [user_instagram] = 'no aplica',
+    [user_x] = 'no aplica',
+    [telefono] = 'sin registro',
+    [genero] = 'n/a',
+    [edad] = 0;
+
+-- 3️⃣ Ahora sí los convertimos a NOT NULL
+ALTER TABLE [dbo].[usuarios]
+ALTER COLUMN [user_facebook] VARCHAR(255) NOT NULL;
+
+ALTER TABLE [dbo].[usuarios]
+ALTER COLUMN [user_instagram] VARCHAR(255) NOT NULL;
+
+ALTER TABLE [dbo].[usuarios]
+ALTER COLUMN [user_x] VARCHAR(255) NOT NULL;
+
+ALTER TABLE [dbo].[usuarios]
+ALTER COLUMN [telefono] VARCHAR(20) NOT NULL;
+
+ALTER TABLE [dbo].[usuarios]
+ALTER COLUMN [genero] VARCHAR(10) NOT NULL;
+
+ALTER TABLE [dbo].[usuarios]
+ALTER COLUMN [edad] INT NOT NULL;
+
+
 /*777 php artisan make:middleware CheckRole*/
+
+  INSERT INTO [dbo].[nivel_usuarios] 
+    ([nombre_rol], [created], [active]) 
+VALUES 
+    ('Admin', GETDATE(), 1);
